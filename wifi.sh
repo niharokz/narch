7da63e042a35f46756006e57af85c2e66bca9416
niharokz <nihars@mail.com>
2021-03-13 13:45:54 +0530

    checl

--- wifi.sh
+++ wifi.sh
@@ -8,7 +8,7 @@
 #       ██║░╚███║██║██║░░██║██║░░██║██║░░██║╚█████╔╝██║░╚██╗███████╗
 #       ╚═╝░░╚══╝╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚══════╝
 #
-#       DRAFTED BY NIHAR SAMANTARAY ON 13-03-21. [https://nihars.com]
+#       DRAFTED BY NIHAR SAMANTARAY ON 10-11-20. [https://nihars.com]
 #       SOURCE [wifi.sh] LAST MODIFIED ON 13-03-21
 
 nmcli device wifi list
@@ -17,3 +17,4 @@ read wifi_name
 echo WIFI password
 read wifi_pass
 nmcli device wifi connect $wifi_name password $wifi_pass 
+
